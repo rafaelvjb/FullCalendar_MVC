ba5b9b3970e9c1d7eacadfd99bfdac2658f0f720
Renan <renancp91@gmail.com>
2017-05-29 20:49:25 -0400

    Auditoria simples implementada

---
+++ FullCalendar_MVC/Models/EventoAuditoria.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FullCalendar_MVC.Models
+{
+    [Table("EventoAuditoria")]
+    public class EventoAuditoria
+    {
+        public int ID { get; set; }
+
+        public String Titulo { get; set; }
+
+        public DateTime DataAntiga { get; set; }
+
+        public DateTime DataNova { get; set; }
+
+        public String UsuarioModificacao { get; set; }
+    }
+}
